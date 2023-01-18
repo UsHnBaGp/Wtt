5f9034adaddbc466c02a7a2922926afe869c1c06
Ditsch <ge84fet@ph.tum.de>
2023-01-18 15:45:45 +0100

    Updated Inversion

---
+++ Kira/results/Tuserweight/kira_WantedTest.kira.m
@@ -0,0 +1,9 @@
+{
+Tuserweight[3] -> 
+ + Tuserweight[2]*(((1))/((y+(-1))*(1)))
+ + Tuserweight[1]*((((-1)))/((y+(-1))*(1)))
+,
+Tuserweight[4] -> 
+ + Tuserweight[2]*((((-1)))/((x)*(y+(-1))*(1)))
+ + Tuserweight[1]*(((y)*(1))/((x)*(y+(-1))*(1)))
+}
