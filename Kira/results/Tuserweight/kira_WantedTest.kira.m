cecdb3e1f70a7596f349578cd0de36d35da79ef5
Ditsch <ge84fet@ph.tum.de>
2023-01-20 14:05:50 +0100

    20.1.

--- Kira/results/Tuserweight/kira_WantedTest.kira.m
+++
@@ -1,9 +0,0 @@
-{
-Tuserweight[3] -> 
- + Tuserweight[2]*(((1))/((y+(-1))*(1)))
- + Tuserweight[1]*((((-1)))/((y+(-1))*(1)))
-,
-Tuserweight[4] -> 
- + Tuserweight[2]*((((-1)))/((x)*(y+(-1))*(1)))
- + Tuserweight[1]*(((y)*(1))/((x)*(y+(-1))*(1)))
-}
