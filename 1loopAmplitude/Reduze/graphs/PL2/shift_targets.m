c02e4054f0103b91179bfca168a7d004eac71907
Sara <sara.ditsch@gmx.de>
2023-02-28 15:28:49 +0100

    shifts

---
+++ 1loopAmplitude/Reduze/graphs/PL2/shift_targets.m
@@ -0,0 +1,5 @@
+{
+{"PL2", 2, 5, MultiGraph[{Labeled[-1->3, "(p1,0;1)"], Labeled[-2->3, "(p2,0;2)"], Labeled[-3->1, "(p3,mt^2;3)"], Labeled[-4->1, "(p4,mt^2;4)"], Labeled[-5->3, "(-p2-p4-p1-p3,mw^2;5)"], Labeled[3->1, "(k1,mt^2;6)"], Labeled[1->3, "(k1+p4+p3,mt^2;8)"]}]},
+{"PL2", 2, 9, MultiGraph[{Labeled[-1->2, "(p1,0;1)"], Labeled[-2->1, "(p2,0;2)"], Labeled[-3->1, "(p3,mt^2;3)"], Labeled[-4->1, "(p4,mt^2;4)"], Labeled[-5->1, "(-p2-p4-p1-p3,mw^2;5)"], Labeled[1->2, "(k1,mt^2;6)"], Labeled[2->1, "(k1+p1,0;9)"]}]},
+{"PL2", 3, 7, MultiGraph[{Labeled[-1->3, "(p1,0;1)"], Labeled[-2->3, "(p2,0;2)"], Labeled[-3->1, "(p3,mt^2;3)"], Labeled[-4->2, "(p4,mt^2;4)"], Labeled[-5->3, "(-p2-p4-p1-p3,mw^2;5)"], Labeled[3->1, "(k1,mt^2;6)"], Labeled[1->2, "(k1+p3,0;7)"], Labeled[2->3, "(k1+p4+p3,mt^2;8)"]}]}
+}
---
+++ 1loopAmplitude/Reduze/graphs/PL3/shift_targets.m
@@ -0,0 +1,9 @@
+{
+{"PL3", 2, 10, MultiGraph[{Labeled[-1->3, "(p1,0;1)"], Labeled[-2->3, "(p2,0;2)"], Labeled[-3->1, "(p3,mt^2;3)"], Labeled[-4->1, "(p4,mt^2;4)"], Labeled[-5->1, "(-p2-p4-p1-p3,mw^2;5)"], Labeled[3->1, "(k1+p1,0;7)"], Labeled[1->3, "(-p2+k1,0;9)"]}]},
+{"PL3", 2, 17, MultiGraph[{Labeled[-1->2, "(p1,0;1)"], Labeled[-2->2, "(p2,0;2)"], Labeled[-3->1, "(p3,mt^2;3)"], Labeled[-4->2, "(p4,mt^2;4)"], Labeled[-5->2, "(-p2-p4-p1-p3,mw^2;5)"], Labeled[2->1, "(k1,0;6)"], Labeled[1->2, "(k1+p3,0;10)"]}]},
+{"PL3", 2, 24, MultiGraph[{Labeled[-1->3, "(p1,0;1)"], Labeled[-2->1, "(p2,0;2)"], Labeled[-3->1, "(p3,mt^2;3)"], Labeled[-4->3, "(p4,mt^2;4)"], Labeled[-5->3, "(-p2-p4-p1-p3,mw^2;5)"], Labeled[3->1, "(-p2+k1,0;9)"], Labeled[1->3, "(k1+p3,0;10)"]}]},
+{"PL3", 3, 11, MultiGraph[{Labeled[-1->2, "(p1,0;1)"], Labeled[-2->3, "(p2,0;2)"], Labeled[-3->1, "(p3,mt^2;3)"], Labeled[-4->1, "(p4,mt^2;4)"], Labeled[-5->1, "(-p2-p4-p1-p3,mw^2;5)"], Labeled[3->2, "(k1,0;6)"], Labeled[2->1, "(k1+p1,0;7)"], Labeled[1->3, "(-p2+k1,0;9)"]}]},
+{"PL3", 3, 14, MultiGraph[{Labeled[-1->3, "(p1,0;1)"], Labeled[-2->3, "(p2,0;2)"], Labeled[-3->2, "(p3,mt^2;3)"], Labeled[-4->2, "(p4,mt^2;4)"], Labeled[-5->1, "(-p2-p4-p1-p3,mw^2;5)"], Labeled[3->1, "(k1+p1,0;7)"], Labeled[1->2, "(-p2+k1-p4-p3,0;8)"], Labeled[2->3, "(-p2+k1,0;9)"]}]},
+{"PL3", 3, 25, MultiGraph[{Labeled[-1->3, "(p1,0;1)"], Labeled[-2->1, "(p2,0;2)"], Labeled[-3->2, "(p3,mt^2;3)"], Labeled[-4->3, "(p4,mt^2;4)"], Labeled[-5->3, "(-p2-p4-p1-p3,mw^2;5)"], Labeled[1->2, "(k1,0;6)"], Labeled[3->1, "(-p2+k1,0;9)"], Labeled[2->3, "(k1+p3,0;10)"]}]},
+{"PL3", 4, 15, MultiGraph[{Labeled[-1->2, "(p1,0;1)"], Labeled[-2->4, "(p2,0;2)"], Labeled[-3->3, "(p3,mt^2;3)"], Labeled[-4->3, "(p4,mt^2;4)"], Labeled[-5->1, "(-p2-p4-p1-p3,mw^2;5)"], Labeled[4->2, "(k1,0;6)"], Labeled[2->1, "(k1+p1,0;7)"], Labeled[1->3, "(-p2+k1-p4-p3,0;8)"], Labeled[3->4, "(-p2+k1,0;9)"]}]}
+}
